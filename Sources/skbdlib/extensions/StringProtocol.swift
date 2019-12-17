a8c62b8a1d3c10204c6779050cfd3622c4de489c
Tom Bell <tomb@tomb.io>
2019-12-17 21:19:38 +0000

    Add StringProtocol extension functions and tests

---
+++ Sources/skbdlib/extensions/StringProtocol.swift
@@ -0,0 +1,12 @@
+public extension StringProtocol {
+    subscript(_ offset: Int) -> Element {
+        self[index(startIndex, offsetBy: offset)]
+    }
+
+    subscript(_ range: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: Swift.max(0, range.lowerBound))
+        let end = index(startIndex, offsetBy: Swift.min(self.count, range.upperBound))
+
+        return String(self[start..<end])
+    }
+}
---
+++ Tests/skbdlibTests/extensions/StringProtocolTests.swift
@@ -0,0 +1,23 @@
+
+import XCTest
+
+import skbdlib
+
+final class StringProtocolTests: XCTestCase {
+    func testStringCharacterIndexing() {
+        let str = "Hello world 😍"
+
+        XCTAssertEqual(str[1], "e")
+        XCTAssertEqual(str[5], " ")
+        XCTAssertEqual(str[8], "r")
+        XCTAssertEqual(str[12], "😍")
+    }
+
+    func testStringRangeIndexing() {
+        let str = "Hello world 🤦🏻‍♂️"
+
+        XCTAssertEqual(str[0..<5], "Hello")
+        XCTAssertEqual(str[6..<11], "world")
+        XCTAssertEqual(str[12..<13], "🤦🏻‍♂️")
+    }
+}
