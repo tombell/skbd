ddac40babbc43d545d292d62d766f13cb1ba6677
Tom Bell <tomb@tomb.io>
2019-12-17 22:39:49 +0000

    Add additional types to TokenType

--- Sources/skbdlib/config/TokenType.swift
+++ Sources/skbdlib/config/TokenType.swift
@@ -1,7 +1,15 @@
 public enum TokenType {
     case endOfStream
     case unknown
+
     case comment
+    case identifier
+
+    case command
+    case modifier
+    case key
+    case literal
+
     case plus
     case dash
 }
