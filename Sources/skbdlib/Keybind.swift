73e77dac14234cb679bec8447c1b654909d4770a
Tom Bell <tomb@tomb.io>
2019-12-17 21:39:40 +0000

    Add carbon keycode/modifiers init to Keybind

--- Sources/skbdlib/Keybind.swift
+++ Sources/skbdlib/Keybind.swift
@@ -5,20 +5,22 @@ public typealias Handler = () -> Void
 public class Keybind {
     public let identifier = UUID()
 
-    public var key: String
-    public var modifiers: [String]
-
+    public var carbonKeyCode: UInt32
+    public var carbonModifiers: UInt32
     public var handler: Handler?
 
-    var carbonKeyCode: UInt32
-    var carbonModifiers: UInt32
-
     public init(key: String, modifiers: [String], handler: Handler? = nil) {
-        self.key = key
-        self.modifiers = modifiers
-
         carbonKeyCode = Key.code(for: key)
         carbonModifiers = Modifier.flags(for: modifiers)
+
+        self.handler = handler
+
+        KeybindController.register(keybind: self)
+    }
+
+    public init(carbonKeyCode: UInt32, carbonModifiers: UInt32, handler: Handler? = nil) {
+        self.carbonKeyCode = carbonKeyCode
+        self.carbonModifiers = carbonModifiers
 
         self.handler = handler
 
