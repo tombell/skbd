e8ea45d04a7f2cc6e8d7698e1c8fc9ae1129fb50
Tom Bell <tomb@tomb.io>
2019-12-19 01:14:17 +0000

    wip: parser

--- Sources/skbdlib/config/Parser.swift
+++ Sources/skbdlib/config/Parser.swift
@@ -1,1 +1,48 @@
-public class Parser {}
+public class Parser {
+    private var lexer: Lexer
+
+    private var currToken: Token?
+
+    public init(buffer: String) {
+        lexer = Lexer(buffer)
+    }
+
+    // Check if the end of file has been reached.
+    private func isEndOfFile() -> Bool {
+        guard let token = currToken else {
+            return false
+        }
+
+        return token.type == .endOfStream
+    }
+
+    // Advance to the next token.
+    private func advance() {
+        if !isEndOfFile() {
+            currToken = lexer.getToken()
+        }
+    }
+
+    // Check if the current token matches the given token type.
+    private func check(type: TokenType) -> Bool {
+        if isEndOfFile() {
+            return false
+        }
+
+        guard let token = currToken else {
+            return false
+        }
+
+        return token.type == type
+    }
+
+    // Check if the current token matches the given token type, and advance the token if it does.
+    private func match(type: TokenType) -> Bool {
+        if check(type: type) {
+            advance()
+            return true
+        }
+
+        return false
+    }
+}
