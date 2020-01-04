8138d72380e65f9bbebdad4de0d01bb45b440a22
Tom Bell <tomb@tomb.io>
2020-01-04 15:25:33 +0000

    Add usage text, first pass

--- Sources/skbd/main.swift
+++ Sources/skbd/main.swift
@@ -10,8 +10,19 @@ var configPath: String = ConfigPath.resolve()
 func main(args: [String]) -> Int32 {
     do {
         guard let arguments = try parseArguments(args) else {
-            // TODO: print help usage
-            print("skbd usage")
+            print("""
+            usage: skbd [arguments]
+
+              --config, -c   the path to the config file
+              --reload, -r   reload the config file
+
+            Special options:
+
+              --help, -h     show this message, then exit
+              --version, -v  show the version number, then exit
+
+            """)
+
             return EXIT_SUCCESS
         }
 
